3cde69d785c8d0d84738e4356cc43058e80f85f8
Honbra <honbraofficial@gmail.com>
2020-11-18 22:28:48 +0100

    Fixed actions

--- src/index.tsx
+++ src/index.tsx
@@ -21,6 +21,8 @@ const editor = new Quill("#editor", {
   },
 });
 
+console.log(editor);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
