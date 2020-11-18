dc520bb46175339e1ab0a5e1ce364757362802c2
Honbra <honbraofficial@gmail.com>
2020-11-18 22:22:30 +0100

    Folders Sidebar

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,13 +3,20 @@ import React from "react";
 function Editor() {
   return (
     <div id="editor" className="content">
-      <p>
-        No, you can't just close the door!
-      </p>
-      <p>
+      <h1>Cat ipsum</h1>
+      {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map(Garbage)}
+    </div>
+  );
+}
+
+function Garbage() {
+  return (
+    <React.Fragment>
+      <p>No, you can't just close the door!</p>
+      <p className="ql-align-right">
         Haha door go <strong>brrrrrrrrrrrrr</strong>
       </p>
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,46 @@
-import React from "react";
-import { ListUl, Folder } from "react-bootstrap-icons";
+import React, { useState } from "react";
+import { ListUl, FileEarmarkText } from "react-bootstrap-icons";
 import Toolbar from "./Toolbar";
 
 function Navbar() {
+  const [foldersOpen, setFoldersOpen] = useState(false);
   return (
-    <nav
-      className="navbar"
-      role="navigation"
-      aria-label="app controls"
-      id="navbar"
-    >
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <div className="navbar-item">
-            <button className="button is-white">
-              <ListUl />
-            </button>
+    <React.Fragment>
+      <nav
+        className="navbar"
+        role="navigation"
+        aria-label="app controls"
+        id="navbar"
+      >
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <div className="navbar-item">
+              <button
+                className="button is-white"
+                onClick={() => setFoldersOpen(!foldersOpen)}
+              >
+                <ListUl />
+              </button>
+            </div>
+          </div>
+
+          <div className="navbar-brand" id="toolbar">
+            <Toolbar />
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <button className="button is-white">
+                <FileEarmarkText />
+              </button>
+            </div>
           </div>
         </div>
-
-        <div className="navbar-brand" id="toolbar">
-          <Toolbar />
-        </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <button className="button is-white">
-              <Folder />
-            </button>
-          </div>
-        </div>
-      </div>
-    </nav>
+      </nav>
+      <nav id="folders" className={"" + (foldersOpen ? "open" : "")}>
+        eeeeeeee
+      </nav>
+    </React.Fragment>
   );
 }
 
