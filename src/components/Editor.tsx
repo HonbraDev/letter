06826429f4d2b369f46bca74a6d04d7112336935
Honbra <honbraofficial@gmail.com>
2020-11-19 17:04:47 +0100

    Lorem ipsum

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,23 +1,7 @@
 import React from "react";
 
 function Editor() {
-  return (
-    <div id="editor" className="content">
-      <h1>Cat ipsum</h1>
-      {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map(Garbage)}
-    </div>
-  );
-}
-
-function Garbage() {
-  return (
-    <React.Fragment>
-      <p>No, you can't just close the door!</p>
-      <p className="ql-align-right">
-        Haha door go <strong>brrrrrrrrrrrrr</strong>
-      </p>
-    </React.Fragment>
-  );
+  return <div id="editor" className="content"></div>;
 }
 
 export default Editor;
